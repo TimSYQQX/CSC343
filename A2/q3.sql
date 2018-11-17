40544d3cbf59ebaeeb16b6164b8f716b9fcb4108
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2018-11-16 20:18:20 -0500

    Edit q3.sql

--- A2/q3.sql
+++ A2/q3.sql
@@ -21,7 +21,7 @@ DROP VIEW IF EXISTS unfit_countries CASCADE;
 
 -- Define views for your intermediate steps here.
 create VIEW participationRatio_in_period
-as select country_id, extract(year from e_date) as year, avg(votes_valid::float / electorate::float) as participationRatio
+as select country_id, extract(year from e_date) as year, avg(votes_cast::float / electorate::float) as participationRatio
 from election
 where extract(year from e_date) >= 2001 and extract(year from e_date) <= 2016 
 group by(country_id, year);
@@ -31,9 +31,9 @@ select distinct country_id
 from participationRatio_in_period;
 
 create VIEW unfit_countries as 
-select distinct p1.country_id, p1.year, p1.participationRatio, p2.country_id as country_id2, p2.year as year2, p2.participationRatio as p
-from participationRatio_in_period p1 cross join participationRatio_in_period p2
-where p1.country_id = p2.country_id and p1.year > p2.year and p1.participationRatio < p2.participationRatio;
+select name, p1.country_id, p1.year, p1.participationRatio, p2.country_id as country_id2, p2.year as year2, p2.participationRatio as p
+from participationRatio_in_period p1,participationRatio_in_period p2, country
+where p1.country_id = p2.country_id and p1.year > p2.year and p1.participationRatio >= p2.participationRatio AND p1.country_id = id;
 
 -- create VIEW 
 -- the answer to the query 
