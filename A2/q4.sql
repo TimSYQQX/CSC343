c737be2aa06f1d3dbe7fd97c44eef54504b57901
Your Name <you@example.com>
2018-11-16 20:38:17 -0500

    q4

--- A2/q4.sql
+++ A2/q4.sql
@@ -26,13 +26,39 @@ from party join party_position on id = party_id
 where left_right is not NULL and country_id is not NULL;
  
 create view r0_2_country as
-select country_id, count(left_right)
+select country_id, count(left_right) as r0_2
 from party_position_with_country
 where left_right <= 2
 group by country_id;
 
-create view r2_4_country as 
-select country_id, count(left_right)
+create view r2_4_country as
+select country_id, count(left_right) as r2_4
+from party_position_with_country
+where left_right > 2 and left_right <= 4
+group by country_id;
+
+create view r4_6_country as
+select country_id, count(left_right) as r4_6
+from party_position_with_country
+where left_right > 4 and left_right <= 6
+group by country_id;
+
+create view r6_8_country as
+select country_id, count(left_right) as r6_8
+from party_position_with_country
+where left_right > 6 and left_right <= 8
+group by country_id;
+
+create view r8_10_country as
+select country_id, count(left_right) as r8_10
+from party_position_with_country
+where left_right > 8 and left_right <= 10;
+group by country_id;
+
+create view coutry_interval AS
+select country_id, r0_2, r2_4, r4_6, r6_8, r8_10
+from r0_2_country join r4_6_country join r4_6_country join r6_8_country join r8_10_country
+
 -- the answer to the query 
 -- INSERT INTO q4 
 
