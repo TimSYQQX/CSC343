76dc1b278fb7b1005f3e64c826108607bc5ec9a4
xiong <tianyu.xiong@mail.utoronto.ca>
2018-11-16 21:26:48 -0500

    q3

--- A2/q3.sql
+++ A2/q3.sql
@@ -15,9 +15,9 @@ create table q3(
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS intermediate_step CASCADE;
 DROP VIEW IF EXISTS participationRatio_in_period CASCADE;
-DROP VIEW IF EXISTS full_countries CASCADE;
-DROP VIEW IF EXISTS unfit_countries CASCADE;
-
+--DROP VIEW IF EXISTS full_countries CASCADE;
+DROP VIEW IF EXISTS fit_countries CASCADE;
+drop view if exists final_countries CASCADE;
 
 -- Define views for your intermediate steps here.
 create VIEW participationRatio_in_period
@@ -26,16 +26,20 @@ from election
 where extract(year from e_date) >= 2001 and extract(year from e_date) <= 2016 
 group by(country_id, year);
 
-create VIEW all_countries as
+create VIEW fit_countries as 
 select distinct country_id
-from participationRatio_in_period;
+from participationRatio_in_period
+except
+select distinct p1.country_id
+from participationRatio_in_period p1,participationRatio_in_period p2
+where p1.country_id = p2.country_id and p1.year > p2.year and p1.participationRatio < p2.participationRatio;
 
-create VIEW unfit_countries as 
-select name, p1.country_id, p1.year, p1.participationRatio, p2.country_id as country_id2, p2.year as year2, p2.participationRatio as p
-from participationRatio_in_period p1,participationRatio_in_period p2, country
-where p1.country_id = p2.country_id and p1.year > p2.year and p1.participationRatio >= p2.participationRatio AND p1.country_id = id;
-
--- create VIEW 
+create VIEW final_countries as
+select fit_countries.country_id, year, participationRatio
+from fit_countries join participationRatio_in_period on fit_countries.country_id = participationRatio_in_period.country_id;
 -- the answer to the query 
--- insert into q3 
+insert into q3 
+select name, year, participationRatio 
+from final_countries join country on country_id = id;
+
 
